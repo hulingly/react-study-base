dc9ea6d92d4133230da8abb5623b842df1931d2f
hulingly <hulingly@163.com>
2019-08-18 21:45:57 +0800

    react基础学习

---
+++ json/data.js
@@ -0,0 +1,35 @@
+var dataList = {
+    friend: {
+        name: "好友",
+        list: [
+            { username: "好友333", message: "啦啦啦", vip: false },
+            { username: "好友444", message: "啦啦啦", vip: false },
+            { username: "好友555", message: "啦啦啦", vip: false },
+            { username: "好友111", message: "啦啦啦", vip: false },
+            { username: "好友666", message: "啦啦啦", vip: false },
+            { username: "好友222", message: "啦啦啦", vip: false }
+        ]
+    },
+    family: {
+        name: "家人",
+        list: [
+            { username: "家人111", message: "啦啦啦", vip: false },
+            { username: "家人222", message: "啦啦啦", vip: false },
+            { username: "家人333", message: "啦啦啦", vip: false },
+            { username: "家人444", message: "啦啦啦", vip: false },
+            { username: "家人555", message: "啦啦啦", vip: false },
+            { username: "家人666", message: "啦啦啦", vip: false }
+        ]
+    },
+    colleageus: {
+        name: "同事",
+        list: [
+            { username: "同事111", message: "啦啦啦", vip: false },
+            { username: "同事222", message: "啦啦啦", vip: false },
+            { username: "同事333", message: "啦啦啦", vip: false },
+            { username: "同事444", message: "啦啦啦", vip: false },
+            { username: "同事555", message: "啦啦啦", vip: false },
+            { username: "同事666", message: "啦啦啦", vip: true }
+        ]
+    }
+};
